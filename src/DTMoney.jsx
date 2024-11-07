8d1899c82cc62b83df2a347e981fdd0259eeb8b2
iTzGustavoRocha <itzgustax@gmail.com>
2024-11-06 21:06:05 -0300

    header concluido

--- src/DTMoney.jsx
+++ src/DTMoney.jsx
@@ -4,6 +4,7 @@ import "./DTMoney.css";
 
 
 
+
 function DTMoney(){
     return(
        
@@ -15,17 +16,22 @@ function DTMoney(){
             <div className="movimentacoes">
                 <div className="entradas" >
                     
-                    <h5 className="textEntrada">entradas </h5>
-                    <p>{}</p>
+                    <h2 className="textEntrada">Entradas &#11141; </h2>
+                    <p>R$17.400,00</p>
                     
                     
 
                 </div>
                 <div  className="saidas">
-                    <span className="textSaidas">saidas</span>
+                    <h2 className="textSaidas">saidas  &#11141;</h2>
+                    <p>R$1.400,00</p>
+
                 </div>
+
                 <div className="total">
-                    <span className="textTotal">Total</span>
+                    <h2 className="textTotal">Total  &#11141;</h2>
+                    <p>R$16.456,00</p>
+
                 </div>
             </div>
             <form action="">
