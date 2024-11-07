6ef029989e035fba167215683ac4974d82599274
iTzGustavoRocha <itzgustax@gmail.com>
2024-11-07 10:15:47 -0300

    estilizacao cards (movimentacoes e total)

--- src/DTMoney.jsx
+++ src/DTMoney.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./DTMoney.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleArrowUp } from "@fortawesome/free-solid-svg-icons";
+import { faCircleArrowDown } from "@fortawesome/free-solid-svg-icons";
+import { faDollarSign } from "@fortawesome/free-solid-svg-icons";
+
 
 
 
@@ -16,25 +21,29 @@ function DTMoney(){
             <div className="movimentacoes">
                 <div className="entradas" >
                     
-                    <h2 className="textEntrada">Entradas &#11141; </h2>
+                    <h2 className="textEntrada">Entradas  </h2>
+                    <FontAwesomeIcon className="icon" icon={faCircleArrowUp} />
+
                     <p>R$17.400,00</p>
                     
                     
 
                 </div>
                 <div  className="saidas">
-                    <h2 className="textSaidas">saidas  &#11141;</h2>
+                    <h2 className="textSaidas">Saídas  </h2>
+                    <FontAwesomeIcon className="iconSaida" icon={faCircleArrowDown} />
                     <p>R$1.400,00</p>
 
                 </div>
 
                 <div className="total">
-                    <h2 className="textTotal">Total  &#11141;</h2>
+                    <h2 className="textTotal">Total  </h2>
+                    <FontAwesomeIcon className="iconTotal" icon={faDollarSign} />
                     <p>R$16.456,00</p>
 
                 </div>
             </div>
-            <form action="">
+            <form >
                 <input className="barraPesquisaTransacao" type="text" placeholder="Busque uma transação"/>
                 <button className="botaoBuscarTransacao" type="submit"> buscar</button>
             </form>
